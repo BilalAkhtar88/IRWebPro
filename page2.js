a268d947a0560fb7a60a26274d0139b1d005d7f1
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2018-03-22 23:36:44 +0100

    Timer Added

--- page2.js
+++ page2.js
@@ -1,19 +1,25 @@
 var ind = 1;
+var n1 = 0;
+var n2 = 0;
 var startTime, endTime;
 var diffTime;
 
 function start(){
 	startTime = new Date();
+	//n1 = startTime.getTime();
+	//document.getElementById("timer").innerHTML = startTime.getTime();
+	
 }
 
 function end(){
 	endTime = new Date();
-    var diffTime = endTime - startTime;	
-    //diffTime = diffTime/1000;
+	//n2 = endTime.getTime();
+    var diffTime = endTime - startTime ;	
+    diffTime = diffTime / 1000;
     var seconds = Math.round(diffTime);
-    alert(diffTime.toString())
+    //alert(diffTime)
     //document.getElementById("demo").innerHTML = diffTime;
-	document.getElementById("timer").innerHTML = diffTime;
+	document.getElementById("timer").innerHTML = parseInt(diffTime);
 }
 
-start;
+start();
